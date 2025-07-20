bc4a3b0b9bf8431c066be8f5ae0312972a1ebde3
Ayush Redekar <ayushr.builds@gmail.com>
2025-07-20 17:38:46 +0530

    change to check commit

--- JS_basics/let/const/var.js
+++ JS_basics/let/const/var.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountId = "Jaipur"
 let accountState;
 
 // accountId = 2 // not allowed
@@ -13,10 +13,10 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountId, accountState])
